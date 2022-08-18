65238d4eb3132d654b9ed139df0e0b196c9463f6
salim104 <salimshaban885@gmail.com>
2022-08-18 20:52:47 +0200

    added

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-//
+//btn for functioality
 let btn = document.querySelector(".btn");
+// the display component
 let container_2 = document.querySelector(".container-2");
 let container_1 = document.querySelector(".container-1");
+// the element that hold all the numbers
+let numbers = document.querySelectorAll(".number");
+let outNumber = document.querySelector(".out");
 
 
+let value = 5;
 btn.addEventListener("click",containerShow)
+
 
 function containerShow(e) {
    e.preventDefault();
     container_1.style.display = "none";
     container_2.style.display = "block";
-
-    // console.log("clicked me")
+   
+   outNumber.innerHTML = value;
 }
+
+ numbers.forEach(btn => {
+      btn.addEventListener('click',handleRatingBtn)
+     
+ })
+  
+function handleRatingBtn(eve){
+   eve.preventDefault();
+  numbers.forEach(btn => {
+      btn.classList.remove("active");
+  })
+
+  eve.target.classList.add("active");
+  
+}
+
+
